e25bf21ad68b1ee47961588ba5e5b7760b0581e5
Natanael Lima <taelima1997@gmail.com>
2021-11-21 22:01:47 -0300

    add implamantation example redux tool kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux'
+
 import styles from "./App.module.scss";
 import api from "./services/api";
+import { decrement, incrementAsync, selectCount } from "./store/slices/couterSlice";
 
 function App() {
+    const count = useSelector(selectCount)
+    const dispatch = useDispatch()
+
   useEffect(() => {
     async function getPlants() {
       try {
@@ -21,6 +27,22 @@ function App() {
     <div className="App">
       <main className={styles.contentWrapper}>
         <h1>PlantCrud</h1>
+
+        <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementAsync(2))}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
       </main>
     </div>
   );

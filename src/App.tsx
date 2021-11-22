c1d0de03f773fb0a95b6fad805038bce30d69188
Natanael Lima <taelima1997@gmail.com>
2021-11-21 21:33:05 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <main className={styles.contentWrapper}>
+        <h1>PlantCrud</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;

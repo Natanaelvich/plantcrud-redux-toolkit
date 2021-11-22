dd23a2135397834540a2e8f72f17e4b5754953e3
Natanael Lima <taelima1997@gmail.com>
2021-11-21 21:41:56 -0300

    add getplants

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import styles from "./App.module.scss";
+import api from "./services/api";
 
 function App() {
+  useEffect(() => {
+    async function getPlants() {
+      try {
+        const response = await api.get("plants");
+
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getPlants();
+  }, []);
+
   return (
     <div className="App">
       <main className={styles.contentWrapper}>

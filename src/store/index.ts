4f96067fd3022157146daecc2189f9718dbb35f1
Natanael Lima <taelima1997@gmail.com>
2021-11-22 23:54:21 -0300

    add plantreducer to configure store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./slices/couterSlice";
+import plantsReducer from "./slices/plantsSlice";
 
 export const store = configureStore({
-  reducer: { counter: counterReducer },
+  reducer: { counter: counterReducer, plants : plantsReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;

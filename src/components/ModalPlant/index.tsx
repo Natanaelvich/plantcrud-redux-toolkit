87a5711a6c65ec92c3f908e7bfccf57cc0113c8d
Natanael Lima <taelima1997@gmail.com>
2021-12-01 23:39:18 -0300

    update with delete plant

--- src/components/ModalPlant/index.tsx
+++ src/components/ModalPlant/index.tsx
@@ -1,41 +1,50 @@
-import React from 'react';
-import {MdClose, MdDelete, MdEdit} from 'react-icons/md'
-import { Plant } from '../../store/slices/plantsSlice';
+import React from "react";
+import { MdClose, MdDelete, MdEdit } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { deletePlantAsync, Plant } from "../../store/slices/plantsSlice";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 type Props = {
-handleClose : () => void
-plant : Plant
-}
+  handleClose: () => void;
+  plant: Plant;
+};
 
-const ModalPlant : React.FC<Props> = ({handleClose, plant}) => {
-    return (
-        <div className={styles.modalContainer}>
-            <div className={styles.modalContent}>
-                <button onClick={handleClose}>
-                    <MdClose />
-                </button>
-                <img src={plant.photo} alt="plant=photo" width={100} height={100} />
-                <h1>{plant.name}</h1>
+const ModalPlant: React.FC<Props> = ({ handleClose, plant }) => {
+  const dispatch = useDispatch();
 
-                <h3>Sobre</h3>
-                <p>{plant.about}</p>
+  function handleDeletePlant() {
+    dispatch(deletePlantAsync(plant.id));
 
-                <h3>Dicas de água</h3>
-                <p>{plant.water_tips}</p>
+    handleClose();
+  }
 
-                <div>
-                    <button>
-                        <MdEdit />
-                    </button>
-                    <button>
-                        <MdDelete />
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.modalContent}>
+        <button onClick={handleClose}>
+          <MdClose />
+        </button>
+        <img src={plant.photo} alt="plant=photo" width={100} height={100} />
+        <h1>{plant.name}</h1>
+
+        <h3>Sobre</h3>
+        <p>{plant.about}</p>
+
+        <h3>Dicas de água</h3>
+        <p>{plant.water_tips}</p>
+
+        <div>
+          <button>
+            <MdEdit />
+          </button>
+          <button onClick={handleDeletePlant}>
+            <MdDelete />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ModalPlant;

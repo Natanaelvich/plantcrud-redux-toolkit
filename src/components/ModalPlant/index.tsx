415417fc59aada2659b34b873c6b8c547d84848e
Natanael Lima <taelima1997@gmail.com>
2021-12-04 23:57:12 -0300

    add edit plant component flux

--- src/components/ModalPlant/index.tsx
+++ src/components/ModalPlant/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
-import { MdClose, MdDelete, MdEdit } from "react-icons/md";
+import React, { useState } from "react";
+import {
+  MdCancel,
+  MdCheck,
+  MdCheckCircle,
+  MdClose,
+  MdDelete,
+  MdEdit,
+} from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
 import {
   deletePlantAsync,
+  editPlantAsync,
   Plant,
   selectPlants,
 } from "../../store/slices/plantsSlice";
@@ -17,10 +25,20 @@ type Props = {
 
 const ModalPlant: React.FC<Props> = ({ handleClose, plant }) => {
   const dispatch = useDispatch();
-  const { loadingDelete } = useSelector(selectPlants);
+  const { loadingDelete, loadingEdit } = useSelector(selectPlants);
+
+  const [showEditMode, setShowEditMode] = useState(false);
+  const [about, setAbout] = useState(plant.about);
+  const [waterTips, setWaterTips] = useState(plant.water_tips);
 
   function handleDeletePlant() {
     dispatch(deletePlantAsync(plant.id, handleClose));
+  }
+
+  function handleEditPlant() {
+    const plantEdit = { ...plant, about, waterTips };
+
+    dispatch(editPlantAsync(plantEdit, () => setShowEditMode(false)));
   }
 
   return (
@@ -33,18 +51,46 @@ const ModalPlant: React.FC<Props> = ({ handleClose, plant }) => {
         <h1>{plant.name}</h1>
 
         <h3>Sobre</h3>
-        <p>{plant.about}</p>
+        {showEditMode ? (
+          <textarea
+            value={about}
+            onChange={(value) => setAbout(value.target.value)}
+          />
+        ) : (
+          <p>{about}</p>
+        )}
 
         <h3>Dicas de água</h3>
-        <p>{plant.water_tips}</p>
+
+        {showEditMode ? (
+          <textarea
+            value={waterTips}
+            onChange={(value) => setWaterTips(value.target.value)}
+          />
+        ) : (
+          <p>{waterTips}</p>
+        )}
 
         <div>
-          <button>
-            <MdEdit />
-          </button>
-          <button onClick={handleDeletePlant}>
-            {loadingDelete ? <LoadingCircle /> : <MdDelete />}
-          </button>
+          {showEditMode ? (
+            <>
+              <button onClick={handleEditPlant}>
+                {loadingEdit ? <LoadingCircle /> : <MdCheck />}
+              </button>
+              <button onClick={() => setShowEditMode(false)}>
+                <MdClose />
+              </button>
+            </>
+          ) : (
+            <>
+              <button onClick={() => setShowEditMode(true)}>
+                <MdEdit />
+              </button>
+              <button onClick={handleDeletePlant}>
+                {!loadingDelete ? <LoadingCircle /> : <MdDelete />}
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
